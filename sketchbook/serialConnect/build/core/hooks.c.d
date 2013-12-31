56f3a35bad2596882ef81920362f3cd594a64d17
Ektor5 <ektor-5@udoo.org>
2013-12-31 02:23:24 +0100

    delete binaries

--- sketchbook/serialConnect/build/core/hooks.c.d
+++
@@ -1,2 +0,0 @@
-/home/olga/Arduino/sketchbook/serialConnect/build/core/hooks.c.o: \
- /opt/arduino-1.5.4/hardware/arduino/sam//cores/arduino/hooks.c
--- sketchbook/serialConnect/build/core/iar_calls_sam3.c.d
+++
@@ -1,2 +0,0 @@
-/home/olga/Arduino/sketchbook/serialConnect/build/core/iar_calls_sam3.c.o: \
- /opt/arduino-1.5.4/hardware/arduino/sam//cores/arduino/iar_calls_sam3.c
